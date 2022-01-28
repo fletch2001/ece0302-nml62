289148ad68f19ac59911540d724910fe87825155
nml-mac <nml@nml-mbp-mac.local>
2022-01-27 19:52:43 -0500

    updated HW2

--- hw2/abstract_bag.hpp
+++ hw2/abstract_bag.hpp
@@ -4,18 +4,25 @@
 template<typename T> class abstract_bag{
 public:
 
+  //gets current size
   virtual std::size_t getCurrentSize() const = 0;
 
+  //returns if empty or not
   virtual bool isEmpty() const = 0;
 
+  //adds entry to bag
   virtual bool add(const T& entry) = 0;
 
+  //removes instance of entry from bag
   virtual bool remove(const T& entry) = 0;
 
+  //clears bag
   virtual void clear() = 0;
 
+  //gets frequency of items occurring in bag
   virtual std::size_t getFrequencyOf(const T& entry) const = 0;
 
+  //returns whether bag contains at least one instance of an item or not
   virtual bool contains(const T& entry) const = 0;
 
 };
