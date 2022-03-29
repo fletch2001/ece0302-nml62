3e0887abee5cd5fe1da16f5790e6307e5c0bd8cb
nml-mac <nml@nml-mbp-mac.local>
2022-03-29 13:10:25 -0400

    working on p4

--- p4_starter_code/pathfinder.cpp
+++ p4_starter_code/pathfinder.cpp
@@ -8,18 +8,18 @@ struct coord{
   unsigned int j;
 };
 
-struct exit_path {
-  unsigned int steps;
-  coord c;
+struct metadata {
+  std::string outputfile;
+  Image<Pixel> map;
 };
 
-bool find_start(Image<Pixel> &, coord &);
-void search(Image<Pixel> &, Image<Pixel> &, const coord &, std::string);
+bool find_start(metadata &, coord &);
+void search(metadata &, Image<Pixel> &, const coord &);
 coord previousRow(coord);
 coord nextRow(coord);
 coord previousColumn(coord);
 coord nextColumn(coord);
-void exit_handler(Image<Pixel> &);
+void exit_handler(int, metadata);
 
 int nodes = 0;
 
@@ -28,7 +28,9 @@ int main(int argc, char *argv[])
   Image<Pixel> map = readFromFile(argv[1]); // read map in from file to Image object
   Image<Pixel> explored = map; // explored map to keep track of explored coordinates
 
-  std::string outputfile = argv[2];
+  metadata problemData{argv[2], map};
+  // problemData.outputfile = argv[2];
+  // problemData.map = map;
 
   unsigned width, height;
   
@@ -41,46 +43,41 @@ int main(int argc, char *argv[])
 //*** j is column
 
   // finding start point (red sqaure)
-  if(find_start(map, start)) {
-    std::cout << start.i << " " << start.j << std::endl;
+  if(find_start(problemData, start)) {
+    //std::cout << start.i << " " << start.j << std::endl;
     //std::cout << map(start.j, start.i).alpha << std::endl << map(start.i, start.j).blue << std::endl << map(start.i, start.j).green << std::endl << map(start.i, start.j).red << std::endl;
-    std::cout << (map(start.i, start.j) == RED) <<"\n\n\n\n";
+   // std::cout << (map(start.i, start.j) == RED) <<"\n\n\n\n";
 
-    search(map, explored, start, outputfile);
+    search(problemData, explored, start);
   }
 }
 
 // takes a coordinate by reference and 
-bool find_start(Image<Pixel> &map, coord &c) {
-  for(unsigned j = 0; j < map.height(); j++) {
-      for(unsigned i = 0; i < map.width(); i++) {
-        if(map(i, j) == RED) { // start search here
+bool find_start(metadata &m, coord &c) {
+  bool start_found = 0;
+
+  for(unsigned j = 0; j < m.map.height(); j++) {
+      for(unsigned i = 0; i < m.map.width(); i++) {
+        if(m.map(i, j) != BLACK && m.map(i, j) != WHITE && m.map(i, j) != RED ) {
+          exit_handler(-2, m);
+        } else if(m.map(i, j) == RED && start_found == false) { // start search here
           c = {i, j};
-          return true; // exit from search if found
+          start_found = true; // exit from search if found
+        } else if(m.map(i, j) == RED && start_found == true) {
+          exit_handler(-2, m);
         }
       }
     }
     return false;
 }
 
-void search(Image<Pixel> &map, Image<Pixel> &explored, const coord & c, std::string outputfile) {
-  //if(map(c.i, c.j) == WHITE) map(c.i, c.j) = BLUE;
-  if(map(c.i, c.j) == BLACK) std::cout << c.j << " " << c.i << " B\n";
-  if(map(c.i, c.j) == WHITE) std::cout << c.j << " " << c.i << " W\n";
-    //std::cout << map(c.i, c.j).alpha << std::endl << map(c.i, c.j).blue << std::endl << map(c.i, c.j).green << std::endl << map(c.i, c.j).red << std::endl;
-
-  // if(c.i == 0 || c.j == 0 || c.j == map.width() - 1 || c.i == map.height() - 1 && (map(c.i, c.j) == WHITE)) { // case when exit is found
-  //   std::clog << "EXIT FOUND\n\n";
-  //   map(c.i, c.j) = GREEN; // print exit
-  //   writeToFile(map, outputfile);
-  //   exit_handler(map); // go to exit handler
-  //   return;
-  // }
+void search(metadata &m, Image<Pixel> &explored, const coord & c) {
+  Image<Pixel> map = m.map; // extract map from metadata struct
 
   //explored(c.i, c.j) = BLUE; // set explored node to blue
   
   Deque<coord> frontier;
-
+  frontier.pushFront(c);
 /*
 
 pR
@@ -92,68 +89,61 @@ nC
 
   //explored(c.i, c.j) = BLUE;
 
-  if(c.i >= 1) {
-    if(map(c.i - 1, c.j) == WHITE && explored(c.i - 1, c.j) == WHITE) {
-      std::cout << "q:" << frontier.isEmpty() << "\n";
-      frontier.pushFront(previousRow(c)); // add previous row to frontier
-      //explored(c.i, c.j + 1) = Pixel{0, 0, static_cast<uint8_t>(120 + nodes), 255};
-      writeToFile(explored, "explored.png");
-    }
-  }
-
-  if(c.i < map.height() - 1) {
-    if(map(c.i + 1, c.j) == WHITE && explored(c.i + 1, c.j) == WHITE) {
-      std::cout << "q:" << frontier.isEmpty() << "\n";
-      frontier.pushFront(nextRow(c));
-      //explored(c.i, c.j + 1) = Pixel{0, 0, static_cast<uint8_t>(120 + nodes), 255};
-      writeToFile(explored, "explored.png");
-    } 
-  }
-
-  if(c.j >= 1) {
-    if(map(c.i, c.j - 1) == WHITE && explored(c.i, c.j - 1) == WHITE) {
-      std::cout << "q:" << frontier.isEmpty() << "\n";
-      frontier.pushFront(previousColumn(c));
-      //explored(c.i, c.j + 1) = Pixel{0, 0, static_cast<uint8_t>(120 + nodes), 255};
-      //explored(c.i, c.j + 1) = Pixel{0, 0, 120, 255};
-      writeToFile(explored, "explored.png");
-    } 
-  }
-
-  if(c.j < map.width() - 1) {
-    if(map(c.i, c.j + 1) == WHITE && explored(c.i, c.j + 1) == WHITE) {
-      std::cout << "q:" << frontier.isEmpty() << "\n";
-      frontier.pushFront(nextColumn(c));
-      //explored(c.i, c.j + 1) = Pixel{0, 0, 120, 255};
-      writeToFile(explored, "explored.png");
-    } 
-  }
   
   // writeToFile(map, outputfile);
   
 
-  while(frontier.isEmpty() == false) {
+  while(true) {
     std::clog << "search\n";
     //std::cout << frontier.back().i << " " << frontier.back().j << std::endl;
      //search(map, explored, frontier.back(), outputfile); // check first node in queue
     
-    coord current = frontier.back;
+      if(frontier.isEmpty() == true) {
+        exit_handler(-1, m);
+      }
 
-     explored(frontier.back().i, frontier.back().j) = BLUE;
+      coord current = frontier.back();
+      frontier.popBack(); // remove checked node from list
+      explored(current.i, current.j) = BLUE;
 
-      if(c.i == 0 || c.j == 0 || c.j == map.width() - 1 || c.i == map.height() - 1 && (map(c.i, c.j) == WHITE)) { // case when exit is found
-  //   std::clog << "EXIT FOUND\n\n";
-  //   map(c.i, c.j) = GREEN; // print exit
-  //   writeToFile(map, outputfile);
-  //   exit_handler(map); // go to exit handler
-  //   return;
-  // }
+      if(current.i == 0 || current.j == 0 || current.j == map.width() - 1 || current.i == map.height() - 1 && (map(current.i, current.j) == WHITE)) { // case when exit is found
+        //std::clog << "EXIT FOUND\n\n";
+        map(current.i, current.j) = GREEN; // print exit
+        exit_handler(1, metadata{m.outputfile, map}); // go to exit handler (pass in 1 for success)
+      }
 
+      if(current.i >= 1) { // check that not on edge and that map(prevrow) will not throw error
+          if(map(current.i - 1, current.j) == WHITE && explored(current.i - 1, current.j) == WHITE) {
+            //std::cout << "q:" << frontier.isEmpty() << "\n";
+            frontier.pushFront(previousRow(current)); // add previous row to frontier
+            // std::cout << previousRow(current).i << " " << previousRow(current).j << " pushed to queue\n";
+          }
+        }
 
-     frontier.popBack(); // remove checked node from list
+        if(current.i < map.height() - 1) { // check that map() will not go out of range and throw error
+          if(map(current.i + 1, current.j) == WHITE && explored(current.i + 1, current.j) == WHITE) {
+            //std::cout << "q:" << frontier.isEmpty() << "\n";
+            frontier.pushFront(nextRow(current));
+            // std::cout << nextRow(current).i << " " << nextRow(current).j << " pushed to queue\n";
+          } 
+        }
+
+        if(current.j >= 1) { // check that map() will not go out of range and throw error
+          if(map(current.i, current.j - 1) == WHITE && explored(current.i, current.j - 1) == WHITE) {
+            //std::cout << "q:" << frontier.isEmpty() << "\n";
+            frontier.pushFront(previousColumn(current));
+            // std::cout << previousColumn(current).i << " " << previousColumn(current).j << " pushed to queue\n";
+          } 
+        }
+
+        if(current.j < map.width() - 1) {
+          if(map(current.i, current.j + 1) == WHITE && explored(current.i, current.j + 1) == WHITE) {
+            //std::cout << "q:" << frontier.isEmpty() << "\n";
+            frontier.pushFront(nextColumn(current));
+            // std::cout << nextColumn(current).i << " " << nextColumn(current).j << " pushed to queue\n";
+          } 
+        }
   }
-
-  return;
 }
 
 coord previousRow(coord c) {
@@ -172,6 +162,20 @@ coord nextColumn(coord c) {
   return coord{c.i, c.j + 1};
 }
 
-void exit_handler(Image<Pixel> & map) {
-  exit(EXIT_SUCCESS);
+void exit_handler(int e, metadata m) {
+  if(e == 1) {
+    writeToFile(m.map, m.outputfile); // write output to map
+    std::cout << "solution found\n";
+    exit(0);
+  }
+  else if(e == -1) {
+    writeToFile(m.map, m.outputfile);
+    std::cout << "no solution found\n";
+    exit(1);
+  }
+  else if(e == -2) {
+    std::cerr << "error\n";
+    writeToFile(m.map, m.outputfile);
+    exit(1);
+  }
 }
